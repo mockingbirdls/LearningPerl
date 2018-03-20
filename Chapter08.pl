719965136f216bf6dc8d10a14efcb6dd7370a4b3
mockingbirdls <loradsotirova@gmail.com>
2018-03-20 21:58:22 +0200

    Regular expressions

--- Chapter08.pl
+++ Chapter08.pl
@@ -19,8 +19,19 @@ while (<>) {
     }
     
 }
+say "Ex04: named groups:";
+while (<>) {
+        chomp;
+    if (/(?<group1>\w+a\b)/) {
+        print  "Matched: <group1> contains $+{group1}\n ";
+    }else {
+        print "No match: |$_|\n";
+    }
+    
+}
 
-say "Ex04: match again 'a' word but with 5 symbols after that";
+
+say "Ex05: match again 'a' word but with 5 symbols after that";
 while (<>) {
         chomp;
     if (/(\w+a\b)(.{0,5})/) {
@@ -31,6 +42,16 @@ while (<>) {
     
 }
 
+say "Ex06: match empty spaces in the end of a string ";
+while (<>) {
+    chomp;
+    if (/\s+$/) {
+        say "S_|";
+    }else{
+        say "No match";
+    }
+    
+}
 
 
 sub match {
